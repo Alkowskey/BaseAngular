a60f08f4065425d4c410f393c69388fe7c5e284f
Aleksander Kulinski <aleksanderkulinski@MacBook-Pro-IT.local>
2021-12-06 09:29:11 +0100

    Updated PersonValidation, used asyncValidationFn

--- src/app/_validation/PersonValidation.ts
+++ src/app/_validation/PersonValidation.ts
@@ -1,16 +1,29 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import { PersonInput } from '../../interfaces';
-export const isValidFirstName = (): ValidatorFn => {
-    return (control: AbstractControl): ValidationErrors | null =>
-        control.value?.length > 0
-            ? null : { strLen: control.value.length };
+import { AbstractControl, AsyncValidatorFn, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+
+//Just for the sake of playing with async
+async function playAsync(firstname: string): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (firstname === "Marek") {
+                const err = { forbiddenFirstname: { value: "Forbidden firstname" } };
+                reject(err);
+            }
+
+            resolve(null);
+        }, 1000);
+    });
 }
 
-export const isValidPhone = (): ValidatorFn => {
-    return (control: AbstractControl): ValidationErrors | null => {
+export class PersonValidation {
+    static isValidPhone(control: AbstractControl): ValidationErrors | null {
         const isValid = /^\+?[0-9]{3}-?[0-9]{6,12}$/.test(control.value)
 
         return !isValid ? { forbiddenPhone: { value: control.value } } : null
-    }
+    };
 
-};
+    static async isValidAsync(control: AbstractControl): Promise<ValidationErrors | null> {
+        const reponse = await playAsync(control.value);
+        return reponse
+    }
+}
--- src/app/admin-api/admin-api.component.ts
+++ src/app/admin-api/admin-api.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { PersonInput } from '../../interfaces';
-import { isValidFirstName, isValidPhone } from '../_validation/PersonValidation';
+import { PersonValidation } from '../_validation/PersonValidation';
 
 @Component({
   selector: 'app-admin-api',
@@ -15,9 +15,9 @@ export class AdminAPIComponent implements OnInit {
     { id: 2, firstName: "Przemek", lastName: "Asdd", phone: "123123123", address: "Krk", message: "" },
   ]
   formGroup = new FormGroup({
-    firstName: new FormControl('', [Validators.required, isValidFirstName()]),
+    firstName: new FormControl('', [Validators.required, Validators.minLength(2)], [PersonValidation.isValidAsync]),
     lastName: new FormControl(''),
-    phone: new FormControl('', [Validators.required, isValidPhone()]),
+    phone: new FormControl('', [Validators.required, PersonValidation.isValidPhone]), //I know that there is Validators.pattern but i wanted to write it myself this time
     address: new FormControl(''),
     message: new FormControl(''),
   })
