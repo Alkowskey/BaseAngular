13b20059d39c52b60f2300359d9e0203f2921b9b
Aleksander Kulinski <aleksanderkulinski@MacBook-Pro-IT.local>
2021-12-06 08:11:43 +0100

    Created PermissionGuard that uses query parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import Auth from './_utils/Auth';
 import { AdminAPIComponent } from './admin-api/admin-api.component';
+import { Auth, PermissionGuard } from "./_utils"
 
 //redirect
 const routes: Routes = [
   { path: 'admin', component: AdminAPIComponent, canActivate: [Auth], },
-  { path: 'admin/:id', component: AdminAPIComponent, canActivate: [Auth] },
+  { path: 'admin/:id', component: AdminAPIComponent, canActivate: [Auth, PermissionGuard] },
   { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [Auth]
+  providers: [Auth, PermissionGuard]
 })
 export class AppRoutingModule { }
