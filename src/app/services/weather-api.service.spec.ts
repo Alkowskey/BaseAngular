f9cda8614d2ba338718fcb1e1c84979a6c40ee99
Aleksander Kulinski <aleksanderkulinski@MacBook-Pro-IT.local>
2021-12-14 12:07:34 +0100

    Created test for service

--- src/app/services/weather-api.service.spec.ts
+++ src/app/services/weather-api.service.spec.ts
@@ -2,17 +2,17 @@ import { TestBed } from '@angular/core/testing';
 
 import { WeatherAPIService } from './weather-api.service';
 import { HttpClientModule, HttpClient, HttpHandler } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-
 describe('WeatherAPIService', () => {
   let service: WeatherAPIService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [HttpHandler, HttpClient] });
+    TestBed.configureTestingModule({ imports: [HttpClientModule], providers: [WeatherAPIService] });
     service = TestBed.inject(WeatherAPIService);
   });
 
-  it('should be created', () => {
+  it('should receive data', () => {
+    const data = service.getWeather();
     expect(service).toBeTruthy();
+    expect(data).toBeDefined();
   });
 });
