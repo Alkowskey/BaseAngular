e7f15454c33fed90a416c74e8157499eda574456
Aleksander Kulinski <aleksanderkulinski@ITs-MacBook-Pro.local>
2022-02-10 09:52:29 +0100

    Created observables that provides progress. used async pipe

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,18 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { ProgressService } from '../services/progress.service'
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.sass']
+  styleUrls: ['./progress-bar.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush // let async pipe do the job
 })
 export class ProgressBarComponent implements OnInit {
-  @Input() curr: number = 0;
-  @Input() total: number = 100;
-  progress: number = 0;
-  constructor () {
+  // @Input() curr: number = 0;
+  // @Input() total: number = 100;
+  // progress: number = 0;
+  progre$$: Observable<number>;
+  constructor (private progressService: ProgressService) {
+    this.progre$$ = this.progressService.progress()
   }
 
   ngOnInit (): void {
-    this.progress = this.curr / this.total * 100
+    // this.progress = this.curr / this.total * 100
   }
 }
