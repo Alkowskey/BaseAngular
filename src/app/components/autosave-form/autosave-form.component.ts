d8978d072f7ab1277b6ac5491459cba54f13f8f6
Aleksander Kulinski <aleksanderkulinski@ITs-MacBook-Pro.local>
2022-02-22 14:14:35 +0100

    made status subject

--- src/app/components/autosave-form/autosave-form.component.ts
+++ src/app/components/autosave-form/autosave-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup } from '@angular/forms'
-import { debounceTime, Observable, map, shareReplay, tap } from 'rxjs'
+import { debounceTime, Observable, map, shareReplay, tap, Subject } from 'rxjs'
 import { AutosaveServiceService } from './autosave-service.service'
 
 enum SaveStatus {
@@ -20,16 +20,16 @@ export class AutosaveFormComponent implements OnInit {
     toggle: new FormControl()
   })
 
-  status: SaveStatus = SaveStatus.Idle;
+  status$: Subject<SaveStatus> = new Subject<SaveStatus>();
 
   save$: Observable<string> = this.formGroup.valueChanges.pipe(
     tap(() => {
-      this.status = SaveStatus.Saving
+      this.status$.next(SaveStatus.Saving)
     }),
     debounceTime(1000),
     map((value) => (this.autosaveService.save(value))),
     tap(() => {
-      this.status = SaveStatus.Saved
+      this.status$.next(SaveStatus.Saved)
     }),
     shareReplay(1)
   )
